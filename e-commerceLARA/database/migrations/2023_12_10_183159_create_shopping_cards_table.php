c57ff7437814fd9aedba2a5188ed8e6e0959b469
AhmadShamouka <shamoukaahmad@gmail.com>
2023-12-10 20:32:10 +0200

    Cards

--- e-commerceLARA/database/migrations/2023_12_10_183159_create_shopping_cards_table.php
+++ e-commerceLARA/database/migrations/2023_12_10_183159_create_shopping_cards_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('shoppin_cards', function (Blueprint $table) {
+        Schema::create('shopping_cards', function (Blueprint $table) {
             $table->id();
-            $table->string('product_name');
-            $table->integer('quantity');
-            $table->integer('total_price');
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('shoppin_cards');
+        Schema::dropIfExists('shopping_cards');
     }
 };
